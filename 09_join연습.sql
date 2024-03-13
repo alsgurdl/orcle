4776f1d1f52ce2ea2ff5ead89d1573e9f4226770
kmh <fdf98952@gmail.com>
2024-03-13 11:00:08 +0900

    3/13

--- 09_join연습.sql
+++ 09_join연습.sql
@@ -8,6 +8,7 @@ select*from EMPLOYEES;
 select*from DEPARTMENTS;
 select*from jobs;
 select*from JOB_HISTORY;
+select*from LOCATIONS;
 select
 *
 from EMPLOYEES e
@@ -96,3 +97,79 @@ cross join DEPARTMENTS;
 
 select * from EMPLOYEES ,
 DEPARTMENTS;
+
+/*
+문제 7.
+--EMPLOYEES 테이블과 DEPARTMENTS 테이블의 부서번호를 조인하고 SA_MAN 사원만의 사원번호, 이름, 
+급여, 부서명, 근무지를 출력하세요. as(Alias를 사용)
+*/
+select 
+e.EMPLOYEE_ID as 사원번호,
+e.first_name,
+e.SALARY,
+d.DEPARTMENT_NAME,
+loc.city
+from EMPLOYEES e
+join DEPARTMENTS d
+on e.department_id = d.department_id
+join locations loc
+on d.location_id = loc.location_id
+where e.job_id = 'SA_MAN';
+
+
+
+/*
+문제 8.
+-- employees, jobs 테이블을 조인 지정하고 job_title이 'Stock Manager', 'Stock Clerk'인 
+직원 정보만 출력하세요.
+*/
+select
+*
+from employees e
+ join jobs j
+on e.JOB_ID = j.JOB_ID
+where job_title in ('Stock Manager','Stock Clerk');
+
+/*
+문제 9.
+-- departments 테이블에서 직원이 없는 부서를 찾아 출력하세요. LEFT OUTER JOIN 사용
+*/
+select
+d.department_name
+from departments d
+LEFT OUTER JOIN employees e
+on d.department_id = e.department_id
+where e.employee_id is not NULL
+;
+
+/*
+문제 10. 
+-join을 이용해서 사원의 이름과 그 사원의 매니저 이름을 출력하세요
+힌트) EMPLOYEES 테이블과 EMPLOYEES 테이블을 조인하세요.
+*/
+select 
+e1.FIRST_NAME,
+e2.FIRST_NAME
+from EMPLOYEES e1
+join EMPLOYEES e2
+on e1.manager_id = e2.manager_id;
+
+
+/*
+문제 11. 
+-- EMPLOYEES 테이블에서 left join하여 관리자(매니저)id와, 매니저의 이름, 매니저의 급여 까지 출력하세요.
+-- 매니저 아이디가 없는 사람은 배제하고 급여는 역순으로 정렬하세요.
+*/
+select 
+e1.EMPLOYEE_id,
+e1.FIRST_NAME,
+e1.manager_id,
+e2.FIRST_NAME,
+e2.salary
+from EMPLOYEES e1
+left join EMPLOYEES e2
+on e1.manager_id = e2.EMPLOYEE_id
+where e1.manager_id is not null
+order by e2.salary desc;
+
+
